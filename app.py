7566f59acbd926b88fa09b05863a37407b53cd2f
ebarreto-mendoza <ebarreto-mendoza@ucmerced.edu>
2022-11-10 21:02:20 -0800

    Init Admin

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from flask import abort, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-
-
 import json
 
 app = Flask(__name__)
